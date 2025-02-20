1659642cc0309dee98277f578ac19ccc75349cc4
Andy Kasbarian <kasbarian@g.ucla.edu>
2025-02-20 10:14:31 -0700

    Very crude playable version
    This is the first original code for the game. It implements a level plane,
    differently colored Phong materials for objects, and a very primitive
    Hakkun model made using wireframe shapes and a point light.
    
    Ground movement still needs improvement, but vertical movement and
    gravity has been implemented. You can move jankily using WASD and jump
    smoothly using SPACE.
    
    More features will be implemented very soon.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-
 const scene = new THREE.Scene();
 
 //THREE.PerspectiveCamera( fov angle, aspect ratio, near depth, far depth );
@@ -29,11 +28,7 @@ scene.add(yAxis);
 scene.add(zAxis);
 
 
-// ***** Assignment 2 *****
 // Setting up the lights
-const pointLight = new THREE.PointLight(0xffffff, 100, 100);
-pointLight.position.set(5, 5, 5); // Position the light
-scene.add(pointLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0.5, .0, 1.0).normalize();
@@ -42,189 +37,63 @@ scene.add(directionalLight);
 const ambientLight = new THREE.AmbientLight(0x505050);  // Soft white light
 scene.add(ambientLight);
 
-const phong_material = new THREE.MeshPhongMaterial({
-    color: 0x00ff00, // Green color
+//defined colored phong materials for blocks and Hakkun
+const red_material = new THREE.MeshPhongMaterial({
+    color: 0xff0000, // Red color
     shininess: 100   // Shininess of the material
 });
+const blue_material = new THREE.MeshPhongMaterial({
+    color: 0x0000ff, // Blue color
+    shininess: 100   
+});
+const yellow_material = new THREE.MeshPhongMaterial({
+    color: 0xffff00, // Yellow color
+    shininess: 100   
+});
+const grass_material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00, // Green color
+    shininess: 100   
+});
 
-
-// Start here.
-
+//Blocks have a size of 1x1x1
 const l = 0.5
-const positions = new Float32Array([
-    // Front face
-    -l, -l,  l, // 0
-     l, -l,  l, // 1
-     l,  l,  l, // 2
-    -l,  l,  l, // 3
+const cube_geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    // Left face
-    -l, -l, -l, // 4
-    -l, -l,  l, // 5
-    -l,  l,  l, // 6 
-    -l,  l, -l, // 7
-  
-    // Top face
-	-l,  l,  l, //8
-	 l,  l,  l, //9
-	 l,  l, -l, //10
-	-l,  l, -l, //11
-  
-    // Bottom face
-	-l, -l, -l, //12
-	 l, -l, -l, //13
-	 l, -l,  l, //14
-	-l, -l,  l, //15
-  
-    // Right face
-	 l, -l,  l, //16
-	 l, -l, -l, //17
-	 l,  l, -l, //18
-	 l,  l,  l, //19
+const level_plane_geometry = new THREE.PlaneGeometry(100,100);
 
-     // Back face
-	 l, -l, -l, //20
-	-l, -l, -l, //21
-	-l,  l, -l, //22
-	 l,  l, -l  //23
-  ]);
-  
-  const indices = [
-    // Front face
-    0, 1, 2,
-    0, 2, 3,
-  
-    // Left face
-    4, 5, 6,
-    4, 6, 7,
-  
-    // Top face
-	8, 9, 10,
-	8, 10, 11,
-  
-    // Bottom face
-	12, 13, 14,
-	12, 14, 15,
-  
-    // Right face
-	16, 17, 18,
-	16, 18, 19,
+//Define Hakkun model geometry (NOT DONE)
+const hakkun_head_geometry = new THREE.SphereGeometry(0.5, 6, 6);
+const hakkun_body_geometry = new THREE.CapsuleGeometry(0.35, 0.5, 2, 8); //0.60
 
-    // Back face
-	20, 21, 22,
-	20, 22, 23
-  ];
-  
-  // Compute normals
-  const normals = new Float32Array([
-    // Front face
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-  
-    // Left face
-    -1, 0, 0,
-    -1, 0, 0,
-    -1, 0, 0,
-    -1, 0, 0,
-  
-    // Top face
-	0, 1, 0,
-	0, 1, 0,
-	0, 1, 0,
-	0, 1, 0,
-  
-    // Bottom face
-	0, -1, 0,
-	0, -1, 0,
-	0, -1, 0,
-	0, -1, 0,
-  
-    // Right face
-	1, 0, 0,
-	1, 0, 0,
-	1, 0, 0,
-	1, 0, 0,
+//Hakkun very slightly lights up his surroundings
+const HakkunLight = new THREE.PointLight(0xffffff, 1, 100);
+//HakkunLight.position.set(0, 4, 0); // Position the light
+scene.add(HakkunLight);
 
-    // Back face
-	0, 0, -1,
-	0, 0, -1,
-	0, 0, -1,
-	0, 0, -1
-  ]);
+//let hakkun_head_fill = new THREE.Mesh( hakkun_head_geometry, red_material );
+let hakkun_head_wire = new THREE.LineSegments( hakkun_head_geometry );
+//hakkun_head_fill.position.set(0, 5, 0);
+//hakkun_head_wire.position.set(0, 5, 0);
+//scene.add(hakkun_head_fill);
+scene.add(hakkun_head_wire);
 
-const custom_cube_geometry = new THREE.BufferGeometry();
-custom_cube_geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-custom_cube_geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
-custom_cube_geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
+//let hakkun_body_fill = new THREE.Mesh( hakkun_body_geometry, red_material );
+let hakkun_body_wire = new THREE.LineSegments( hakkun_body_geometry );
+//hakkun_body_fill.position.set(0, 4, 0);
+//hakkun_body_wire.position.set(0, 4, 0);
+//scene.add(hakkun_body_fill);
+scene.add(hakkun_body_wire);
 
-//let cube = new THREE.Mesh( custom_cube_geometry, phong_material );
-//scene.add(cube);
-
-// Implement wireframe geometry
-const wireframe_vertices = new Float32Array([
-	// Front face
-    -l, -l,  l,
-     l, -l,  l,
-     l, -l,  l,
-     l,  l,  l,
-     l,  l,  l,
-    -l,  l,  l,
-    -l,  l,  l,
-	-l, -l,  l,
-    // Left face
-    -l, -l, -l,
-    -l, -l,  l,
-	-l, -l,  l,
-    -l,  l,  l,
-    -l,  l,  l,
-	-l,  l, -l,
-	-l,  l, -l,
-	-l, -l, -l,
-    // Top face
-	-l,  l,  l,
-	 l,  l,  l,
-	 l,  l,  l,
-	 l,  l, -l,
-	 l,  l, -l,
-	-l,  l, -l,
-	-l,  l, -l,
-	-l,  l,  l,
-    // Bottom face
-	-l, -l, -l,
-	 l, -l, -l,
-	 l, -l, -l,	
-	 l, -l,  l,
-	 l, -l,  l,
-	-l, -l,  l,
-	-l, -l,  l,
-	-l, -l, -l,
-    // Right face
-	 l, -l,  l,
-	 l, -l, -l,
-	 l, -l, -l,
-	 l,  l, -l,
-	 l,  l, -l,
-	 l,  l,  l,
-	 l,  l,  l,
-	 l, -l,  l,
-     // Back face
-	 l, -l, -l,
-	-l, -l, -l,
-	-l, -l, -l,
-	-l,  l, -l,
-	-l,  l, -l,
-	 l,  l, -l,
-	 l,  l, -l,
-	 l, -l, -l
-]);
-
-const wireframe_geometry = new THREE.BufferGeometry();
-wireframe_geometry.setAttribute( 'position', new THREE.BufferAttribute( wireframe_vertices, 3 ) );
+//The level plane
+let level = new THREE.Mesh(level_plane_geometry, grass_material);
+level.matrixAutoUpdate = false;
+level.matrix.copy(rotationMatrixX(3*Math.PI/2));
+scene.add(level);
+level.material.color.setRGB(0,1,0);
 
 //const line = new THREE.LineSegments( wireframe_geometry );
 
+//Transformation matrices:
 function translationMatrix(tx, ty, tz) {
 	return new THREE.Matrix4().set(
 		1, 0, 0, tx,
@@ -233,7 +102,25 @@ function translationMatrix(tx, ty, tz) {
 		0, 0, 0, 1
 	);
 }
-// Other transformation functions:
+
+function rotationMatrixX(theta) {
+	return new THREE.Matrix4().set(
+		1, 0,                0,               0,
+		0, Math.cos(theta), -Math.sin(theta), 0,
+		0, Math.sin(theta),  Math.cos(theta), 0,
+		0, 0,                0,               1
+	);
+}
+
+function rotationMatrixY(theta) {
+	return new THREE.Matrix4().set(
+		Math.cos(theta), 0, Math.sin(theta), 0,
+		0,               1, 0,               0,
+	   -Math.sin(theta), 0, Math.cos(theta), 0,
+		0,               0, 0,               1
+	);
+}
+
 function rotationMatrixZ(theta) {
 	return new THREE.Matrix4().set(
 		Math.cos(theta), -Math.sin(theta), 0, 0,
@@ -252,10 +139,11 @@ function scalingMatrix(sx, sy, sz) {
 	);
 }
 
+/*
 //making 2 different arrays for mesh and wireframe cubes:
 let meshCubes = [];
 for (let i = 0; i < 7; i++) {
-	let cube = new THREE.Mesh(custom_cube_geometry, phong_material);
+	let cube = new THREE.Mesh(cube_geometry, yellow_material);
 	cube.matrixAutoUpdate = false;
 	meshCubes.push(cube);
 	scene.add(cube);
@@ -270,7 +158,7 @@ for (let i = 0; i < 7; i++) {
 	//wireframe cubes are not visible by default
 	cube.visible = false;
 }
-
+*/
 // Transform cubes:
 /*
 const S = scalingMatrix(1, 1.5, 1);
@@ -287,7 +175,7 @@ for (let i = 0; i < cubes.length; i++) {
 	model_transformation.premultiply(T2);
 }
 */
-
+/*
 let animation_time = 0;
 let delta_animation_time;
 let rotation_angle;
@@ -295,12 +183,32 @@ const clock = new THREE.Clock();
 
 const MAX_ANGLE = 10 * Math.PI/180
 const T = 2
+*/
+let hakkunX = 0;
+let hakkunY = 4;
+let hakkunZ = 0;
+let hakkunV = 0; //vertical velocity
+const hakkunA = 0.003; //vertical acceleration
 
 function animate() {
     
 	renderer.render( scene, camera );
     controls.update();
 
+	//Gravity: (WIP, the ground is currently hardcoded)
+	if (hakkunY - (hakkunV + hakkunA) > 0){
+		hakkunV += hakkunA;
+		hakkunY -= hakkunV;
+	}
+	else {
+		hakkunV = 0;
+	}
+	
+	//Hakkun body parts positioning:
+	HakkunLight.position.set(hakkunX, hakkunY + 0.7, hakkunZ);
+	hakkun_head_wire.position.set(hakkunX, hakkunY + 1.6, hakkunZ);
+	hakkun_body_wire.position.set(hakkunX, hakkunY + 0.6, hakkunZ);
+	/*
     // Animate the cube
 	delta_animation_time = clock.getDelta();
 	// The animation time only advances when still is false
@@ -308,13 +216,14 @@ function animate() {
 		animation_time += delta_animation_time;
 	}
 	rotation_angle = MAX_ANGLE * (Math.sin(animation_time * 2 * Math.PI / T) + 1) / 2;
+	*/
 	//(sin(x) + 1) / 2 makes a sin function that oscillates between 0 and 1
 	//sin(x * 2pi/T) makes a sin function that oscillates with a period of T
 	//A * (sin(x * 2pi/T) + 1) / 2 has period T and oscillates between 0 and A
 	//x = animation_time and A = MAX_ANGLE
-	
-	const S = scalingMatrix(1, 1.5, 1);
-	const h = 1.5*l //height of "cubes" after scaling
+	/*
+	const S = scalingMatrix(1, 1, 1);
+	const h = l //height of "cubes" after scaling
 	const T1 = translationMatrix(l, h, 0);
 	const R = rotationMatrixZ(rotation_angle);
 	const T2 = translationMatrix(-l, h, 0)
@@ -330,7 +239,7 @@ function animate() {
 		model_transformation.premultiply(R);
 		model_transformation.premultiply(T2);
 	}
-
+	*/
 }
 renderer.setAnimationLoop( animate );
 
@@ -338,8 +247,12 @@ renderer.setAnimationLoop( animate );
 let still = false;
 window.addEventListener('keydown', onKeyPress);
 
+//FIX later: make WASD keypress recognition and movement smoother
+//Possibly implement XZ velocity?
+//Also make Hakkun face a certain direction
 function onKeyPress(event) {
 	switch (event.key) {
+		/*
 		case 's':
 			still = !still;
 			break;
@@ -349,6 +262,24 @@ function onKeyPress(event) {
 				meshCubes[i].visible = !meshCubes[i].visible
 				wireCubes[i].visible = !wireCubes[i].visible
 			}
+			*/
+		case 'w':
+			hakkunZ -= 0.1;
+			break;
+		case 'a':
+			hakkunX -= 0.1;
+			break;
+		case 's':
+			hakkunZ += 0.1;
+			break;
+		case 'd':
+			hakkunX += 0.1;
+			break;
+		case ' ':
+			if (hakkunY - (hakkunV + hakkunA) <= 0){
+				hakkunV = -0.2;
+			}
+			break;
 		default:
 	console.log(`Key ${event.key} pressed`);
 	}
